580a295d5a455fc98e4060cffa53180c6720fdf5
Harsh on Windows7(LandisGyr) <harsh.kumar@landisgyr.com>
2018-01-22 15:01:19 +0530

    RepeatingNameinString
    Adding Project - RepeatingNameinString
    
    Identification:001
    Change-Id: If6b8c68dd14c73571af35247ccf99a211a6b426f

---
+++ RepeatingNameinString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingNameinString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first, str = "abc xyz pqr pqr xyz abc var pqr pqr";
+            var List = str.Split(' ').ToList();
+            first = "";
+            for (int x = 0; x < List.Count; x++)
+            {
+                if (x > 0)
+                    first = List[x - 1];
+
+                if (first == List[x])
+                {
+                    List.Remove(first);
+                    List.Remove(first);
+                    x-=2;
+                }
+            }
+            //Print
+            foreach (var item in List)
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+}
