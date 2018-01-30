6c6ed2917f19c22cf94491919103ec4de073bd5b
Harsh on Windows7(LandisGyr) <harsh.kumar@landisgyr.com>
2018-01-30 19:31:59 +0530

    24 to 12 Hour Format
    Changing time from 24 to 12 Hour Format and vice versa.
    
    Identification:001
    Change-Id: I6ddce02ea00c09cc949d85ff21cc423a7e0e42fc

---
+++ 12HourFormat_To_24HourFormat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12HourFormat_To_24HourFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample InputS -  11:45:26PM || 00:45:26AM
+            string time = "11:45:26PM"; //Console.ReadLine();
+            string AmPm = time.Substring(8, 2);//AM or PM
+            int hh = Convert.ToInt32(time.Substring(0, 2));
+
+            if (AmPm == "AM")
+            {
+                if (hh == 12)
+                    Console.Write(string.Format("00" + time.Substring(2, 6)));
+                else
+                    Console.Write(string.Format(time.Substring(0, 8)));
+            }
+            else
+            {
+                if (hh == 12)
+                    Console.Write(string.Format(time.Substring(0, 8)));
+                else
+                    Console.Write(string.Format((hh + 12) + time.Substring(2, 6)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24HourFormat_To_12HourFormat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourFormat_To_12HourFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample input - 00:15 || 23:19 || 11:59
+            string inputtime = "11:19";
+            Console.WriteLine(Convert24to12Hour(inputtime));
+        }
+
+        private static string Convert24to12Hour(string inputTime)
+        {
+            string result = string.Empty;
+            int amPm;
+            Int32.TryParse(inputTime.Substring(0, 2),out amPm);
+            if(amPm>12)
+                result = String.Format("{0}:{1}:PM", amPm - 12, inputTime.Substring(3, 2));
+            else
+                result = String.Format("{0}:{1}:AM", amPm, inputTime.Substring(3, 2));
+
+            return result;
+        }
+
+    }
+}
