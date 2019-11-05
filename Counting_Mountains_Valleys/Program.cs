ce482bac0d47911094a4819ae9ad26bb5d7f4002
Harsh Kumar <harsh007kumar@gmail.com>
2019-11-06 01:23:06 +0530

    adding better Function to count valley's

--- Counting_Mountains_Valleys/Program.cs
+++ Counting_Mountains_Valleys/Program.cs
@@ -11,12 +11,12 @@ namespace Counting_Mountains_Valleys
         static void Main(string[] args)
         {
             int numberOfSteps = 8;
-            string path = "UDDDUDUU";
-            int result = countingValleys(numberOfSteps, path);
+            string path = "UDDDUDUU";//UDDDUDUUDUDDUDUU
+            int result = countingValleys_2(numberOfSteps, path);
             Console.WriteLine("Total no of valley(s) Gary covered : " + result);
             Console.ReadKey();
         }
-        // Function to count valley's
+        // 1st attempt, Function to count valley's
         private static int countingValleys(int numberOfSteps, string path)
         {
             int current_level = 0, noOfValleys = 0, noOfMountains = 0;
@@ -43,6 +43,28 @@ namespace Counting_Mountains_Valleys
                         current_level++;
                     else if(step == 'D')
                         current_level--;
+                }
+            }
+            return noOfValleys;
+        }
+        // 2nd attempt, Function to count valley's
+        private static int countingValleys_2(int numberOfSteps, string path)
+        {
+            int current_level = 0, noOfValleys = 0, noOfMountains = 0;
+            char[] UpHillDownHill = path.ToCharArray();
+            foreach (char step in UpHillDownHill)
+            {
+                if (step == 'U')
+                {
+                    if (current_level == 0)
+                        noOfMountains++;
+                    current_level++;
+                }
+                else
+                {
+                    if (current_level == 0)
+                        noOfValleys++;
+                    current_level--;
                 }
             }
             return noOfValleys;
