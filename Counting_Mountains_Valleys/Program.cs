086a8325748f165cdb4fc812297b2e10d9fd40cd
Harsh Kumar <harsh007kumar@gmail.com>
2019-11-06 01:01:10 +0530

    CountingMountainsValleys

---
+++ Counting_Mountains_Valleys/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Counting_Mountains_Valleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSteps = 8;
+            string path = "UDDDUDUU";
+            int result = countingValleys(numberOfSteps, path);
+            Console.WriteLine("Total no of valley(s) Gary covered : " + result);
+            Console.ReadKey();
+        }
+        // Function to count valley's
+        private static int countingValleys(int numberOfSteps, string path)
+        {
+            int current_level = 0, noOfValleys = 0, noOfMountains = 0;
+            char[] UpHillDownHill = path.ToCharArray();
+            //Stack<char> GarysSteps = new Stack<char>();
+            foreach(char step in UpHillDownHill)
+            {
+                if(current_level==0)
+                {
+                    if (step == 'U')
+                    {
+                        noOfMountains++;
+                        current_level++;
+                    }
+                    else
+                    {
+                        noOfValleys++;
+                        current_level--;
+                    }
+                }
+                else
+                {
+                    if (step == 'U')
+                        current_level++;
+                    else if(step == 'D')
+                        current_level--;
+                }
+            }
+            return noOfValleys;
+        }
+    }
+}
