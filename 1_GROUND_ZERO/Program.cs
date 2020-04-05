65495264f678b2309d642e15a9425c157be31384
Harsh Kumar <harsh007kumar@gmail.com>
2020-04-06 02:15:24 +0530

    Practising simple Delegate & Event and assinging the event to delegate method handler
    Practising simple Delegate & Event and assinging the event to delegate method handler

---
+++ 1_GROUND_ZERO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_GROUND_ZERO
+{
+    //Custom Delegate
+    public delegate void MyFirstDelegate();
+    class EventDemo
+    {
+        //Custom Event
+        public event MyFirstDelegate MyFirstEvent;
+        public EventDemo()
+        {
+            //Method which matches Signature of Custom Delegate
+            void RegisteredMethod()
+            { Console.WriteLine("RegisteredMethod invoked"); }
+
+            //Registering "RegisteredMethod" method with event
+            MyFirstEvent += new MyFirstDelegate(RegisteredMethod);
+
+            //Firing custom event
+            MyFirstEvent();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new EventDemo();
+            Console.ReadKey();
+        }
+    }
+}
