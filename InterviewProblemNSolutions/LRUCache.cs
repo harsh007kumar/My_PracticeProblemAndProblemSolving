7da0b37dfb674537536150038fdf20d631891ab5
Harsh Kumar <harsh007kumar@gmail.com>
2020-10-16 01:12:06 +0530

    146. LRU Cache
    class Node
    class DLL // Doubly LinkedList
    + Node InsertRear(int data, int key)
    + void Remove(Node node)
    + Node RemoveFront()
    class LRUCache
    + int Get(int key)
    + void Put(int key, int newValue)
    + void Put(int key, int newValue)

---
+++ InterviewProblemNSolutions/LRUCache.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewProblemNSolutions
+{
+    public class Node
+    {
+        public int Data,Key;
+        public Node Prv, Next;
+        public Node(int data, int key)
+        {
+            Data = data;
+            Key = key;
+            Prv = Next = null;
+        }
+    }
+
+    public class DLL                        // Doubly LinkedList
+    {
+        public Node Front, End;
+        public DLL() => Front = End = null; // Constructor
+        public Node InsertRear(int data, int key)
+        {
+            var newNode = new Node(data,key);
+            if (End == null)
+                Front = End = newNode;
+            else
+            {
+                End.Next = newNode;
+                newNode.Prv = End;
+                End = newNode;
+            }
+            return newNode;
+        }
+        public void Remove(Node node)
+        {
+            if (Front == End)
+                Front = End = null;
+            else
+            {
+                if (End == node) End = End.Prv;
+                if (Front == node) Front = Front.Next;
+                if (node.Prv != null) node.Prv.Next = node.Next;
+                if (node.Next != null) node.Next.Prv = node.Prv;
+            }
+        }
+
+        public Node RemoveFront()
+        {
+            var toBeDeleted = Front;
+            if (Front == End)
+                Front = End = null;
+            else
+            {
+                Front = Front.Next;
+                Front.Prv = null;
+            }
+            return toBeDeleted;
+        }
+    }
+
+    public class LRUCache
+    {
+        private Dictionary<int, Node> Dict; // stores the Key in Dict and its corresponding value at CurrCapacity index of the the array which is stored as value in dictionary
+        private DLL LRU;                    // most recently used/added elements are present at the End of the Doubly Linked List
+        public int TotalCapacity;
+        private int currCapacity = 0;
+        public LRUCache(int size)           // constructor
+        {
+            this.TotalCapacity = size;
+            LRU = new DLL();
+            Dict = new Dictionary<int, Node>(TotalCapacity);
+        }
+
+        // Return the value of the key if the key exists, otherwise return -1
+        public int Get(int key)
+        {
+            if (Dict.ContainsKey(key))      // if present return from Dictionary else return -1
+            {
+                // update position to most recent in array
+                var refInList = Dict[key];
+                if (refInList == LRU.End) return LRU.End.Data;
+
+                var node = Dict[key];
+                LRU.Remove(node);
+                Dict[key] = LRU.InsertRear(node.Data,key);
+
+                return node.Data;               // return value which is now stored at lastIndex in array
+            }
+            return -1;
+        }
+
+        // Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+        // If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+        public void Put(int key, int newValue)
+        {
+            if (Dict.ContainsKey(key))
+            {
+                // update value
+                // update key position to most recent in array
+
+                var node = Dict[key];
+                LRU.Remove(node);
+                Dict[key] = LRU.InsertRear(newValue, key);
+            }
+            else
+            {
+                // add to dictionary
+                // add at last index in LinkedList
+
+                // No More Space left, Delete first element from and insert new value at last
+                if (currCapacity >= TotalCapacity)
+                {
+                    var firstNode = LRU.RemoveFront();
+                    Dict.Remove(firstNode.Key);
+                    currCapacity--;
+                }
+
+                Dict.Add(key, LRU.InsertRear(newValue, key));
+                currCapacity++;
+            }
+        }
+    }
+}
--- InterviewProblemNSolutions/StartPoint.cs
+++ InterviewProblemNSolutions/StartPoint.cs
@@ -36,6 +36,7 @@ namespace InterviewProblemNSolutions
             ReorderDataInLogFiles();
             MajorityElement();
             MajorityElementII();
+            LRUCache();
 
             // String Matching Algorithm
             BruteForceWay();
@@ -548,6 +549,23 @@ namespace InterviewProblemNSolutions
                 foreach (var element in result)
                     Console.WriteLine($" Majority Element with more than n/3 majority is: {element}\n");
             }
+        }
+
+        public static void LRUCache()
+        {
+            // https://leetcode.com/problems/lru-cache/
+            Utility.Print("146. LRU Cache");
+
+            LRUCache lRUCache = new LRUCache(2);
+            lRUCache.Put(1, 1); // cache is {1=1}
+            lRUCache.Put(2, 2); // cache is {1=1, 2=2}
+            Console.WriteLine(lRUCache.Get(1));    // return 1
+            lRUCache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+            Console.WriteLine(lRUCache.Get(2));    // returns -1 (not found)
+            lRUCache.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+            Console.WriteLine(lRUCache.Get(1));    // return -1 (not found)
+            Console.WriteLine(lRUCache.Get(3));    // return 3
+            Console.WriteLine(lRUCache.Get(4));    // return 4
         }
 
 
