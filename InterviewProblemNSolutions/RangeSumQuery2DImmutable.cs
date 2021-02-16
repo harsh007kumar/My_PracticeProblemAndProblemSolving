a51ea7a9a9ffbc2971a8c7d8e92e87a1aeae5b31
Harsh Kumar <harsh007kumar@gmail.com>
2021-02-17 02:16:03 +0530

    304. Range Sum Query 2D - Immutable
    class RangeSumQuery2DImmutable
    + public RangeSumQuery2DImmutable(int[][] matrix) // Time O(r*c)
    + int SumRegion(int row1, int col1, int row2, int col2) // Time O(r'), r' = row2-row1

---
+++ InterviewProblemNSolutions/RangeSumQuery2DImmutable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterviewProblemNSolutions
+{
+    public class RangeSumQuery2DImmutable
+    {
+        readonly int[,] mat;
+        // Constructor || Time O(r*c)
+        public RangeSumQuery2DImmutable(int[][] matrix)
+        {
+            int r = matrix.Length;
+            if (r < 1) return;
+            int c = matrix[0].Length;
+            mat = new int[r, c + 1];
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < c; j++)
+                    // Calculate Prefix Sum for each row
+                    mat[i, j + 1] = mat[i, j] + matrix[i][j];
+        }
+
+        // Time O(r'), r' = row2-row1
+        public int SumRegion(int row1, int col1, int row2, int col2)
+        {
+            int sum = 0;
+            for (int r = row1; r <= row2; r++)
+                sum += mat[r, col2 + 1] - mat[r, col1];
+            Console.WriteLine($" Sum of cells b/w top-left corner {row1}:{col1} and bottom-rt corner {row2}:{col2} is: \'{sum}\'");
+            return sum;
+        }
+    }
+}
--- InterviewProblemNSolutions/StartPoint.cs
+++ InterviewProblemNSolutions/StartPoint.cs
@@ -267,6 +267,7 @@ namespace InterviewProblemNSolutions
             SubarraySumEqualsK();
             SubarraySumsDivisibleByK();
             LetterCasePermutation();
+            RangeSumQuery2DImmutable();
 
 
             // String Matching Algorithm
@@ -4404,6 +4405,20 @@ namespace InterviewProblemNSolutions
                 Console.WriteLine("\n");
             }
         }
+
+
+        public static void RangeSumQuery2DImmutable()
+        {
+            // https://leetcode.com/problems/range-sum-query-2d-immutable/
+            Utility.Print("304. Range Sum Query 2D - Immutable");
+            int[][] matrix = { new int[] { 3, 0, 1, 4, 2 }, new int[] { 5, 6, 3, 2, 1 }, new int[] { 1, 2, 0, 1, 5 }, new int[] { 4, 1, 0, 1, 7 }, new int[] { 1, 0, 3, 0, 5 } };
+            matrix.Print("Matrix");
+            RangeSumQuery2DImmutable obj = new RangeSumQuery2DImmutable(matrix);
+            obj.SumRegion(2, 1, 4, 3);  // -> 8
+            obj.SumRegion(1, 1, 2, 2);  // -> 11
+            obj.SumRegion(1, 2, 2, 4);  // -> 12
+        }
+
 
 
 
